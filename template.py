47ddaf49e963b87b90731195c0189a079074f8a8
sachinsen1295 <sachin.sen1295@gmail.com>
2023-10-31 22:21:35 +0530

    files created

--- template.py
+++ template.py
@@ -1,11 +1,12 @@
 import os
-import sys
-import logging
 from pathlib import Path
+import logging
+
 
 logging.basicConfig(level=logging.INFO, format="[%(asctime)s]: %(message)s")
 
-package_name = "CNNClassifier"
+package_name= "CNNClassifier"
+
 
 list_of_files=[
    ".github/workflows/.gitkeep",
@@ -32,18 +33,17 @@ list_of_files=[
                 ]
 
 for filepath in list_of_files:
-    filepath = Path(filepath)
-    file_dir , filename = os.path.split(filepath)
-
-    if file_dir!="":
-        os.makedirs(file_dir,exist_ok=True)
-        logging.info(f"creating directory: {file_dir} for the file: {filename}")
-
+    filepath=Path(filepath)
+    filedir,filename=os.path.split(filepath)
+    
+    if filedir !="":
+        os.makedirs(filedir,exist_ok=True)
+        logging.info(f"creating directory: {filedir} for the file: {filename}")
+        
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
-        with open (filename, "w") as f:
+        with open(filepath,"w") as f:
             pass
-            logging.info(f"creating filepath :{filepath}")
-
-    else :
+            logging.info(f"creating empty file: {filepath}")
+    else:
         logging.info(f"{filepath} already exists")
-
+        
